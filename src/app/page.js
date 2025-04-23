c8ce00a16842cdba68e16f04fe10fc1a9fa884d4
AOD-04 <159972187+AOD-04@users.noreply.github.com>
2025-04-22 20:03:55 -0600

    export default function

--- src/app/page.js
+++ src/app/page.js
@@ -5,5 +5,16 @@ import {useState, useEffect} from "react";
 async function fetchRandomCard() {
   const response = await
   fetch("https://db.ygoprodeck.com/api/v7/cardinfo.php")
-  
+  const data = await response.json();
+  return data;
+}
+
+export default function Page() {
+  const [card, setCard] = useState(null);
+  const [guess, setGuess] = useState("");
+  const [hintNumber, setHintNumber] = useState(0);
+  const [showResult, setShowResult] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [correctCount, setCorrectCount] = useState(0);
+  const [incorrectCount, setIncorrectCount] = useState(0);
 }
