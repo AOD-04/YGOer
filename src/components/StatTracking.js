3892208367c47f851dd4c26398f037d219dc27bb
AOD-04 <159972187+AOD-04@users.noreply.github.com>
2025-04-22 21:23:43 -0600

    Update StatTracking.js

--- src/components/StatTracking.js
+++ src/components/StatTracking.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function ScoreTracker({ correctCount, incorrectCount }) {
+export default function StatTracker({ correctCount, incorrectCount }) {
   return (
     <div>
       <p>Correct: {correctCount} | Incorrect: {incorrectCount}</p>
